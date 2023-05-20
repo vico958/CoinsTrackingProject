ee1b7d1298c2eb3e7ff5873ac833293f4b97be00
vico958 <74486817+vico958@users.noreply.github.com>
2023-05-20 22:46:37 +0300

    checking

--- CoinsTracking/Service/Coins/CoinsManager.cs
+++ CoinsTracking/Service/Coins/CoinsManager.cs
@@ -1,18 +1,17 @@
 ﻿namespace CoinsTracking.Service.Coins
 {
     public class CoinsManager
-    {
+    {//first connectionString it was for working on local at the begging i left it to be here because if i want to change...
         private readonly string connectionString = "Server=.;Database=Coins;Trusted_Connection=True;";
-        private readonly string connectionStringWithAzure = "Server=tcp:coins-server.database.windows.net,1433;Initial Catalog=Coins;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Default;";
-        private readonly string connectionStringWithAzure2 = "Server=tcp:coins-server.database.windows.net,1433;Initial Catalog=Coins;Persist Security Info=False;User ID=viktorDabush;Password=Vd123456789!@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private readonly string connectionStringWithAzure = "Server=tcp:coins-server.database.windows.net,1433;Initial Catalog=Coins;Persist Security Info=False;User ID=viktorDabush;Password=Vd123456789!@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         private readonly CoinsDatabaseManager databaseManager = new CoinsDatabaseManager();
         public async Task<List<CoinsTable>> GetAllCoinsDataAsync()
         {
-            return await databaseManager.GetAllCoinsDataAsync(connectionStringWithAzure2);
+            return await databaseManager.GetAllCoinsDataAsync(connectionStringWithAzure);
         }
         public async Task<bool> UpdateDatabaseAsync(List<CoinsTable> coinsToUpdate)
         {
-            return await databaseManager.UpdateDatabaseAsync(connectionStringWithAzure2, coinsToUpdate);
+            return await databaseManager.UpdateDatabaseAsync(connectionStringWithAzure, coinsToUpdate);
         }
     }
 }
